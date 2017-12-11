aaea716aaeef715af6e61a2c383f7652771590a6
Daniele De Sensi <d.desensi.software@gmail.com>
2017-12-11 16:57:59 +0100

    [IMP] Continuous Integration stuff.

--- src/riff.hpp
+++ src/riff.hpp
@@ -647,7 +647,6 @@ public:
                     tData.sample = ApplicationSample();
                     tData.idleTime = 0;
                     tData.sampleStartTime = now;
-                    // cppcheck-suppress assignBoolToPointer
                     *(tData.consolidate) = false;
                 }
 
