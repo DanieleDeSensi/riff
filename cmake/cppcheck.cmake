36330110a5d6e75430204fc58c134e18c434a3a8
Daniele De Sensi <d.desensi.software@gmail.com>
2019-06-06 15:23:49 +0200

    [FIX] TravisCI stuff.

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -7,7 +7,7 @@ endif()
 
 add_custom_target(
         cppcheck
-        COMMAND ${CPPCHECK_EXECUTABLE} --xml --xml-version=2 --enable=warning,performance,style --error-exitcode=1 --suppressions-list=${PROJECT_SOURCE_DIR}/test/cppcheck/suppressions-list.txt -UNN_EXPORT ${PROJECT_SOURCE_DIR}/src/ ${PROJECT_SOURCE_DIR}/include/ -i${PROJECT_SOURCE_DIR}/src/external -itest 2> cppcheck-report.xml || (cat cppcheck-report.xml && exit 2) 
+        COMMAND ${CPPCHECK_EXECUTABLE} --xml --xml-version=2 --enable=warning,performance,style --error-exitcode=1 --suppressions-list=${PROJECT_SOURCE_DIR}/test/cppcheck/suppressions-list.txt -UNN_EXPORT ${PROJECT_SOURCE_DIR}/src/ ${PROJECT_SOURCE_DIR}/include/ -i${PROJECT_BINARY_DIR} -i${PROJECT_SOURCE_DIR}/src/ -i${PROJECT_SOURCE_DIR}/demo/ -i${PROJECT_SOURCE_DIR}/cmake/ external -itest 2> cppcheck-report.xml || (cat cppcheck-report.xml && exit 2) 
 )
 
 
