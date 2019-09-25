3e1c4f22c51a348fb0c54a98f69eb39104e9c6e9
Daniele De Sensi <d.desensi.software@gmail.com>
2019-09-25 16:47:30 +0000

    [FIX] Arch data generator.

--- src/build_archdata.sh
+++ src/build_archdata.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf archdata.hpp
+rm -rf $1
 
-echo "/*" >> archdata.hpp
-echo " * This file is part of riff" >> archdata.hpp
-echo " *" >> archdata.hpp
-echo " * (c) 2016- Daniele De Sensi (d.desensi.software@gmail.com)" >> archdata.hpp
-echo " *" >> archdata.hpp
-echo " * For the full copyright and license information, please view the LICENSE" >> archdata.hpp
-echo " * file that was distributed with this source code." >> archdata.hpp
-echo " */" >> archdata.hpp
+echo "/*" >> $1
+echo " * This file is part of riff" >> $1
+echo " *" >> $1
+echo " * (c) 2016- Daniele De Sensi (d.desensi.software@gmail.com)" >> $1
+echo " *" >> $1
+echo " * For the full copyright and license information, please view the LICENSE" >> $1
+echo " * file that was distributed with this source code." >> $1
+echo " */" >> $1
 
-echo "#ifndef RIFF_ARCHDATA_HPP_" >> archdata.hpp
-echo "#define RIFF_ARCHDATA_HPP_" >> archdata.hpp
+echo "#ifndef RIFF_ARCHDATA_HPP_" >> $1
+echo "#define RIFF_ARCHDATA_HPP_" >> $1
 
 # Cache line size
 CACHE_LINESIZE=$(cat /sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size)
@@ -22,12 +22,12 @@ if [ "$?" -ne "0" ] || [ "$CACHE_LINESIZE" -eq "0" ]; then
         CACHE_LINESIZE=64
     fi
 fi
-echo "#define LEVEL1_DCACHE_LINESIZE "$CACHE_LINESIZE >> archdata.hpp
+echo "#define LEVEL1_DCACHE_LINESIZE "$CACHE_LINESIZE >> $1
 # Check if constant_tsc is present
 CONSTANT_TSC=$(grep -o '^flags\b.*: .*\bconstant_tsc\b' /proc/cpuinfo | tail -1 | wc -w)
 if [ "$CONSTANT_TSC" -gt "0" ]; then
     VALUE=$(./ticksPerNs)
-    echo "#define RIFF_NS_PER_TICK "$VALUE >> archdata.hpp
+    echo "#define RIFF_NS_PER_TICK "$VALUE >> $1
 fi
 
-echo "#endif // RIFF_ARCHDATA_HPP_" >> archdata.hpp
+echo "#endif // RIFF_ARCHDATA_HPP_" >> $1
